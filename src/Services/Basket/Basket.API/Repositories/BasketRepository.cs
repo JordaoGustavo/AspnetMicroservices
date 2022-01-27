09ef5ea529eccdc1187b9f258d86c58782f049dc
jordaogustavo <jordaogustavo3@gmail.com>
2022-01-26 21:30:55 -0300

    adiciona basket repository e suas implementacoes

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache distributedCache;
+
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache ?? throw new ArgumentException(nameof(distributedCache));
+        }
+
+        public async Task<ShoppingCart?> Get(string userName)
+        {
+            var basket = await distributedCache.GetStringAsync(userName);
+            
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);  
+        }
+
+        public async Task<ShoppingCart?> Update(ShoppingCart basket)
+        {
+            var basketJson = JsonConvert.SerializeObject(basket);
+            await distributedCache.SetStringAsync(basket.UserName, basketJson);
+
+            return await Get(basket.UserName);
+        }
+
+        public async Task Delete(string userName)
+        {
+            await distributedCache.RemoveAsync(userName);
+        }
+    }
+}
