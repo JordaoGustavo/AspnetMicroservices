8bdaf7afef524522a88d4e4de8746e1627c7a158
JordaoGustavo <jordaogustavo3@gmail.com>
2022-07-13 07:20:19 -0300

    :card_file_box: add persistence logic

--- src/Services/Ordering/Ordering.Domain/Common/Entity.cs
+++ src/Services/Ordering/Ordering.Domain/Common/Entity.cs
@@ -6,10 +6,10 @@
 
         public string CreatedBy { get; set; }
 
-        public DateTime CreateDate { get; set; }
+        public DateTime CreatedDate { get; set; }
 
         public string LastModifiedBy { get; set; }
 
-        public DateTime? LasModifiedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
     }
 }
