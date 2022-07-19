6abb08ba7754ec9d2bad5afc73261246702494ad
JordaoGustavo <jordaogustavo3@gmail.com>
2022-07-19 19:51:14 -0300

    :label: adds events entities

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class IntegrationEvent
+    {
+		public Guid Id { get; private set; }
+
+		public DateTime CreationDate { get; private set; }
+
+		public IntegrationEvent()
+		{
+			Id = Guid.NewGuid();
+			CreationDate = DateTime.UtcNow;
+		}
+
+		public IntegrationEvent(Guid id, DateTime createDate)
+		{
+			Id = id;
+			CreationDate = createDate;
+		}
+	}
+}
