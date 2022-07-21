14b4f21baa3888d39f40f8cfe28fbe4d7740d294
JordaoGustavo <jordaogustavo3@gmail.com>
2022-07-20 21:18:40 -0300

    :necktie: adds gateway api business logic

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -1,6 +1,27 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddOcelot();
+
+
+builder.Host.ConfigureLogging((hostingContex, logginBuilder) =>
+{
+    logginBuilder.AddConfiguration(hostingContex.Configuration.GetSection("Logging"));
+    logginBuilder.AddConsole();
+    logginBuilder.AddDebug();
+});
+
+builder.Host.ConfigureAppConfiguration((hostingContex, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContex.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
 
-app.Run();
+await app.RunAsync();
